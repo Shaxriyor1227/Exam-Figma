ac126fe99240308f4da616f3fc6bd659549466ed
Shaxriyor1227 <kiyotasakiayanakoji@gmail.com>
2026-06-06 15:03:30 +0500

    Four commit

--- src/components/Aboutfull/Aboutfull.jsx
+++ src/components/Aboutfull/Aboutfull.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import About2 from '../About/About2'
 import About3 from '../About/About3'
+import Healthy from '../Healthy/Healthy'
+import Comments from '../Comments/Comments'
 
 const Aboutfull = () => {
   return (
     <div>
+        <Healthy />
         <About2/>
         <About3/>
+        <Comments />
     </div>
   )
 }
